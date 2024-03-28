a7bf86ed6336ff8c6975529fc35e8a0627bdc881
zil <zeelpatel2548@gmail.com>
2024-03-28 19:05:46 +0530

    WORK DONE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from "../navbar/navbar.component";
+import {SidebarComponent} from "../sidebar/sidebar.component";
+import {FooterComponent} from "../footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
